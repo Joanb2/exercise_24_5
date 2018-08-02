b135fbf262454b47d14f0cd80ee778a957dfe5ad
Joanna Buchała <joanna.b2@op.pl>
2018-08-02 23:13:59 +0200

    all tests done

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -1,4 +1,22 @@
-const calculateDistacePoints = (distance, hillSizen kPoint) => {
+const calculateDistacePoints = (distance, hillSize, kPoint) => {
+
+	let points = 0;
+
+	switch (hillSize) {
+		case 'normal':
+			points = 60;
+			points = points + (distance - kPoint) * 2;
+			break;
+		case 'big':
+			points = 60;
+			points = points + (distance - kPoint) * 1.8;
+			break;
+		case 'mammoth':
+			points = 120;
+			points = points + (distance - kPoint) * 1.2;
+			break;
+	}
+	return points;
 
 };
 
